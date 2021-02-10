5e7992d3962d6ac405e707e42af7b6abc5a338e6
Shota Ishii <shotaishii@ShotanoMacBook-Pro.local>
2021-02-10 23:13:52 +0900

    tap  event success

--- app/src/main/java/com/example/todo_firebase/MainActivity.kt
+++ app/src/main/java/com/example/todo_firebase/MainActivity.kt
@@ -3,13 +3,11 @@ package com.example.todo_firebase
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import android.widget.ListAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,6 +47,10 @@ class MainActivity : AppCompatActivity() {
                 .addOnFailureListener { exception ->
                     println("Error getting documents: ${exception}")
                 }
+
+        listView.setOnItemClickListener { parent, view, position, id ->
+            println(list[id.toInt()])
+        }
     }
 
 }
