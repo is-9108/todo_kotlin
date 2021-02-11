b4e9465b7224b4d5ba51d06111e251e86bcea905
Shota Ishii <shotaishii@ShotanoMacBook-Pro.local>
2021-02-11 11:54:38 +0900

    delete success

--- app/src/main/java/com/example/todo_firebase/MainActivity.kt
+++ app/src/main/java/com/example/todo_firebase/MainActivity.kt
@@ -15,11 +15,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val fab = findViewById<FloatingActionButton>(R.id.fab)
-
         show()
 
         fab.setOnClickListener{
-            println("tap")
             val intent = Intent(this, SubActivity::class.java)
             startActivity(intent)
         }
@@ -30,18 +28,17 @@ class MainActivity : AppCompatActivity() {
         val listView = findViewById<ListView>(R.id.todoList)
 
         val db = Firebase.firestore
-        var list = mutableListOf<String>()
+        var todoList = mutableListOf<String>()
+        var idList = mutableListOf<String>()
         db.collection("todo")
                 .get()
                 .addOnSuccessListener { result ->
                     for (document in result) {
-                        println("${document.id}: ${document.data}" )
-                        println((document.data).get("todo"))
-                        println((document.data).get("date"))
-                        list.add((document.data).get("todo") as String)
-                        println("LIST: ${list}")
+                        println(document.id)
+                        todoList.add((document.data).get("todo") as String)
+                        idList.add(document.id)
                     }
-                    val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list)
+                    val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,todoList)
                     listView.adapter = adapter
                 }
                 .addOnFailureListener { exception ->
@@ -49,9 +46,26 @@ class MainActivity : AppCompatActivity() {
                 }
 
         listView.setOnItemClickListener { parent, view, position, id ->
-            println(list[id.toInt()])
+            println(todoList[id.toInt()])
+            println(idList[id.toInt()])
+            db.collection("todo").document(idList[id.toInt()]).delete()
+                    .addOnSuccessListener {
+                        println("delete success")
+                        todoList.removeAt(id.toInt())
+                        idList.removeAt(id.toInt())
+                        for (todo in todoList){
+                            println("todo:${todo}")
+                            show()
+                        }
+                        for (id in idList){
+                            println("id: ${id}")
+                        }
+                    }
+                    .addOnFailureListener { e -> println("error: ${e}") }
         }
     }
+
+
 
 }
 
